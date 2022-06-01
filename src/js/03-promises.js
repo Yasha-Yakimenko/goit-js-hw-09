f3aff7f814efe120e2662c27e5752dc52e49cf5b
Yasha-Yakimenko <yasha.yakimenko77@gmail.com>
2022-06-01 19:53:58 +0300

    ,
    ,

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,13 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
+import Notiflix from 'notiflix';
+
+const createPromiseBtn = document.querySelector('button');
+const formElem = document.querySelector('.form');
+      
+      function createPromise(position, delay) { 
+     const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
     // Fulfill
-  } else {
+    } else {
     // Reject
-  }
+   }
 }
