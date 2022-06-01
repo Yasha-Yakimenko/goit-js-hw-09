399e15219fba8e3a1b17925845785d41022fadf4
Yasha-Yakimenko <yasha.yakimenko77@gmail.com>
2022-06-01 12:38:14 +0300

    .
    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyElem = document.querySelector('body');
 
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;  
+    timerId = setInterval(colorSwitcher, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
+ 
+function colorSwitcher() {
+    bodyElem.style.backgroundColor = getRandomHexColor();
+  }
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
