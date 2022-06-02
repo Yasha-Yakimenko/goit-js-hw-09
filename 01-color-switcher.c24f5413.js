b8d32cbe093c423034cc44a1d8fc7b03700119e3
Yasha-Yakimenko <68028093+Yasha-Yakimenko@users.noreply.github.com>
2022-06-02 11:26:31 +0000

    Deploying to gh-pages from @ Yasha-Yakimenko/goit-js-hw-09@e2569731eda967b586894b508b810918244958d6 🚀

--- 01-color-switcher.c24f5413.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");function a(){d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.disabled=!0,e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,timerId=setInterval(a,1e3)})),t.addEventListener("click",(()=>{clearInterval(timerId),e.disabled=!1,t.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.c24f5413.js.map
