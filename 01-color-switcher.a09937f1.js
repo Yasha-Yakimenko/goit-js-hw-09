058ea1baef713e3317eebcb2d855718a5406f7c6
Yasha-Yakimenko <68028093+Yasha-Yakimenko@users.noreply.github.com>
2022-06-02 11:37:57 +0000

    Deploying to gh-pages from @ Yasha-Yakimenko/goit-js-hw-09@1be0d6332562dfcf4c7f2616b5fa8fd0d8c69be5 🚀

--- 01-color-switcher.a09937f1.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body"),n=null;function a(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,n=setInterval(a,1e3)})),t.addEventListener("click",(function(){clearInterval(n),e.disabled=!1,t.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.a09937f1.js.map
---
+++ 01-color-switcher.ff233efb.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");function a(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,timerId=setInterval(a,1e3)})),t.addEventListener("click",(function(){clearInterval(timerId),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.ff233efb.js.map
